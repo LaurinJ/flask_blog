16d39136f487be4965b0c1b21b59545ac0741c57
LaurinJ <laurin223@seznam.cz>
2019-12-27 22:55:05 +0000

    category and single article

--- mdblog/app.py
+++ mdblog/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from flask import render_template, url_for, flash
+from flask import render_template, url_for, request, redirect, flash
 from mdblog.models import db, Article
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired
 from wtforms import StringField, IntegerField, SubmitField, FileField, SelectField, widgets, TextAreaField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Email
 from werkzeug.utils import secure_filename
 from mdblog.models import Article
 
@@ -24,12 +24,20 @@ class ArticleForm(FlaskForm):
     image = FileField("image", validators=[FileRequired()])
     submit = SubmitField("Save")
 
+class NewsletterForm(FlaskForm):
+    email = StringField("email", widget=widgets.Input(input_type="email"),
+                        render_kw=dict(class_="form-control", placeholder="Enter your email"), validators=[InputRequired(), Email()])
+
 @flask_app.route("/")
 def index():
-    image = url_for("static", filename="images/big_img_1.jpg")
-    politics = Article.query.filter_by(category="Politics").paginate(1, 3, False)
-    business = Article.query.filter_by(category="Business").paginate(1, 3, False)
-    return render_template("home.html", image=image, politics=politics.items, business=business.items)
+    page = request.args.get("page", 1, int)
+    editors = Article.query.order_by(Article.id.desc()).limit(7)
+    # trends = Article.query.order_by(Article.id.desc()).limit(4)
+    politics = Article.query.filter_by(category="Politics").order_by(Article.id.desc()).limit(3)
+    business = Article.query.filter_by(category="Business").order_by(Article.id.desc()).limit(3)
+    paginate = Article.query.order_by(Article.id.desc()).paginate(page, 3, False)
+    return render_template("home.html", politics=politics, business=business,
+                           new_articles=paginate.items, paginate=paginate, editors=editors)
 
 @flask_app.route("/add/", methods=["GET", "POST"])
 def add():
@@ -50,3 +58,29 @@ def add():
         flash("Successful")
         return render_template("add.html", form=form)
     return render_template("add.html", form=form)
+
+@flask_app.route("/article/<int:art_id>/", methods=["GET"])
+def view_article(art_id):
+    article = Article.query.filter_by(id=art_id).first()
+    if article:
+        return render_template("article_single.html", article=article)
+    return redirect(url_for("index"))
+
+@flask_app.route("/category/", methods=["GET"])
+def view_category():
+    category = request.args.get("category", "New", str)
+    page = request.args.get("page", 1, int)
+    if category == "New":
+        paginate = Article.query.order_by(Article.id.desc()).paginate(page, 8, False)
+    else:
+        paginate = Article.query.filter_by(category=category).order_by(Article.id.desc()).paginate(page, 8, False)
+    if paginate:
+        return render_template("category.html", articles=paginate.items, paginate=paginate, title=category)
+    return redirect(url_for("index"))
+
+
+@flask_app.context_processor
+def con_form_tre():
+    form = NewsletterForm()
+    trends = Article.query.order_by(Article.id.desc()).limit(4)
+    return {"trends": trends, "form": form}
